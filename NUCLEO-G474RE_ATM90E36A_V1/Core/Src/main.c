5b39a11521ff2b531ab76e9e819ea9a9f302f1f9
victortagayun <vic.tagayun@yahoo.com>
2024-02-29 13:55:54 +0800

    Add NUCLEO-G474RE_ATM90E36A_V1

---
+++ NUCLEO-G474RE_ATM90E36A_V1/Core/Src/main.c
@@ -0,0 +1,534 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <stdio.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define ATM90E3XA_read   0x80
+#define ATM90E3XA_write  0x0
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef hlpuart1;
+
+SPI_HandleTypeDef hspi2;
+DMA_HandleTypeDef hdma_spi2_rx;
+DMA_HandleTypeDef hdma_spi2_tx;
+
+/* USER CODE BEGIN PV */
+
+uint8_t SPI_TXRX_stat;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_LPUART1_UART_Init(void);
+static void MX_SPI2_Init(void);
+/* USER CODE BEGIN PFP */
+
+void VT_Print_Read_ATM90E3XA_address(uint16_t address);
+void VT_Print_Write_ATM90E3XA_address(uint16_t address, uint16_t data);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_LPUART1_UART_Init();
+  MX_SPI2_Init();
+  /* USER CODE BEGIN 2 */
+
+	printf("================================================\n");
+	printf("Starting >> NUCLEO-G474RE_ATM90E36A_V1 \n");
+
+	for(uint16_t address = 0; address < (0x4 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 7; address < (0x1d + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x30; address < (0x3b + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x40; address < (0x4d + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x50; address < (0x57 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x60; address < (0x6f + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x80; address < (0x99 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0xa0; address < (0xcb + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0xd0; address < (0xef + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0xf1; address < (0xff + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x100; address < (0x102 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x11e; address < (0x122 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x13e; address < (0x142 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x15e; address < (0x162 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x17e; address < (0x182 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x15e; address < (0x162 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x19e; address < (0x1a2 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x1be; address < (0x1c5 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	for(uint16_t address = 0x1d0; address < (0x1d1 + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	VT_Print_Read_ATM90E3XA_address(0x216);
+
+	VT_Print_Read_ATM90E3XA_address(0x219);
+
+	VT_Print_Read_ATM90E3XA_address(0x270);
+
+	VT_Print_Read_ATM90E3XA_address(0x27b);
+
+	VT_Print_Read_ATM90E3XA_address(0x2fc);
+
+	VT_Print_Read_ATM90E3XA_address(0x2fd);
+
+	VT_Print_Write_ATM90E3XA_address(0x8, 0xf0f0);
+
+	VT_Print_Write_ATM90E3XA_address(0x9, 0x1234);
+
+	for(uint16_t address = 0xa; address < (0xd + 1); address++)
+		VT_Print_Write_ATM90E3XA_address(address, 0x1111);
+
+	for(uint16_t address = 8; address < (0xd + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	printf("Software Reset ATM90E36A ......... \n");
+	VT_Print_Write_ATM90E3XA_address(0x0, 0x789a);
+
+	for(uint16_t address = 8; address < (0xd + 1); address++)
+		VT_Print_Read_ATM90E3XA_address(address);
+
+	printf("Ending   >> NUCLEO-G474RE_ATM90E36A_V1 \n\n");
+
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+	while (1)
+	{
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		HAL_Delay(100);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	}
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV6;
+  RCC_OscInitStruct.PLL.PLLN = 85;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief LPUART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_LPUART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN LPUART1_Init 0 */
+
+  /* USER CODE END LPUART1_Init 0 */
+
+  /* USER CODE BEGIN LPUART1_Init 1 */
+
+  /* USER CODE END LPUART1_Init 1 */
+  hlpuart1.Instance = LPUART1;
+  hlpuart1.Init.BaudRate = 115200;
+  hlpuart1.Init.WordLength = UART_WORDLENGTH_8B;
+  hlpuart1.Init.StopBits = UART_STOPBITS_1;
+  hlpuart1.Init.Parity = UART_PARITY_NONE;
+  hlpuart1.Init.Mode = UART_MODE_TX_RX;
+  hlpuart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  hlpuart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  hlpuart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  hlpuart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&hlpuart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&hlpuart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&hlpuart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN LPUART1_Init 2 */
+
+  /* USER CODE END LPUART1_Init 2 */
+
+}
+
+/**
+  * @brief SPI2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI2_Init(void)
+{
+
+  /* USER CODE BEGIN SPI2_Init 0 */
+
+  /* USER CODE END SPI2_Init 0 */
+
+  /* USER CODE BEGIN SPI2_Init 1 */
+
+  /* USER CODE END SPI2_Init 1 */
+  /* SPI2 parameter configuration*/
+  hspi2.Instance = SPI2;
+  hspi2.Init.Mode = SPI_MODE_MASTER;
+  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi2.Init.CLKPolarity = SPI_POLARITY_HIGH;
+  hspi2.Init.CLKPhase = SPI_PHASE_2EDGE;
+  hspi2.Init.NSS = SPI_NSS_SOFT;
+  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi2.Init.CRCPolynomial = 7;
+  hspi2.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi2.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  if (HAL_SPI_Init(&hspi2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI2_Init 2 */
+
+  /* USER CODE END SPI2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMAMUX1_CLK_ENABLE();
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+  /* DMA1_Channel2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+void VT_Print_Read_ATM90E3XA_address(uint16_t address)
+{
+	SPI_TXRX_stat = 0;
+	uint8_t ATM90E3XA_TX[4] = {0,0,0,0}, ATM90E3XA_RX[4] = {0,0,0,0};
+
+	ATM90E3XA_TX[0] = ATM90E3XA_read + (uint8_t) (address >> 8);
+	ATM90E3XA_TX[1] = (uint8_t) address;
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, RESET);
+	switch (HAL_SPI_TransmitReceive_DMA(&hspi2, ATM90E3XA_TX, ATM90E3XA_RX, 4))
+	{
+	case HAL_OK: 	  /* Communication is completed ___________________________________________ */
+		break;
+	case HAL_TIMEOUT: /* An Error Occur ______________________________________________________ */
+		printf("SPI HAL_TIMEOUT \n");
+	case HAL_ERROR:   /* Call Timeout Handler */
+		printf("SPI HAL_ERROR \n");
+		Error_Handler();
+		break;
+	default:
+		break;
+	}
+
+	while(!SPI_TXRX_stat){}
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, SET);
+	printf("Read  Address %X%02Xh = %02X%02Xh \n", (ATM90E3XA_TX[0] & 0x7f), ATM90E3XA_TX[1],ATM90E3XA_RX[2],ATM90E3XA_RX[3]);
+
+}
+
+void VT_Print_Write_ATM90E3XA_address(uint16_t address, uint16_t data)
+{
+	SPI_TXRX_stat = 0;
+	uint8_t ATM90E3XA_TX[4] = {0,0,0,0}, ATM90E3XA_RX[4] = {0,0,0,0};
+
+	ATM90E3XA_TX[0] = (uint8_t) (address >> 8);
+	ATM90E3XA_TX[1] = (uint8_t) address;
+	ATM90E3XA_TX[2] = (uint8_t) (data >> 8);
+	ATM90E3XA_TX[3] = (uint8_t) data;
+
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, RESET);
+	switch (HAL_SPI_TransmitReceive_DMA(&hspi2, ATM90E3XA_TX, ATM90E3XA_RX, 4))
+	{
+	case HAL_OK: 	  /* Communication is completed ___________________________________________ */
+		break;
+	case HAL_TIMEOUT: /* An Error Occur ______________________________________________________ */
+		printf("SPI HAL_TIMEOUT \n");
+	case HAL_ERROR:   /* Call Timeout Handler */
+		printf("SPI HAL_ERROR \n");
+		Error_Handler();
+		break;
+	default:
+		break;
+	}
+
+	while(!SPI_TXRX_stat){}
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, SET);
+	printf("Write Address %X%02Xh = %02X%02Xh \n", (ATM90E3XA_TX[0]), ATM90E3XA_TX[1],ATM90E3XA_TX[2],ATM90E3XA_TX[3]);
+
+}
+
+void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
+{
+	/* Prevent unused argument(s) compilation warning */
+	UNUSED(hspi);
+
+	/* NOTE : This function should not be modified, when the callback is needed,
+            the HAL_SPI_RxCpltCallback should be implemented in the user file
+	 */
+
+	//	for(int cntr = 0; cntr < num_data; cntr++)
+	//	{
+	//		printf("ADT7310_RX [%x] = %x \n", cntr, ADT7310_RX[cntr]);
+	//	}
+
+	SPI_TXRX_stat = 1;
+
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	printf("Error_Handler \n");
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, SET);
+	__disable_irq();
+	while (1)
+	{
+	}
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
